d1f8f730ceedefd5445a949f9a705d5675cb6228
smbraden <smbraden@ucsc.edu>
2020-11-27 15:14:07 -0800

    added Dimmer

--- Dimmer/Dimmer/Dimmer.asm
+++ Dimmer/Dimmer/Dimmer.asm
@@ -1,13 +1,13 @@
 /*-------------------------------------------------------------------------------------//
-    Project:			LightMeter
-	Filename:           LightMeter.asm
+    Project:			Dimmer
+	Filename:           Dimmer.asm
     Author:				Sonja Braden
-    Reference:			
-    Date:               11/25/2020
+    Reference:			https://github.com/hexagon5un/AVR-Programming
+    Date:               11/27/2020
 	Device:				ATmega328A
 	Device details:		1MHz clock, 8-bit MCU
-    Description:		Continuously measures light intensity, and represents the 
-						intensity on a scale of 1 to 8, as displayed by a row of LED's
+    Description:		Continuously measures light intensity, and uses PWM to dim/brighten
+						an LED display in response to less/more light intensity on a sensor.
 //-------------------------------------------------------------------------------------*/
 
 .NOLIST								; Don't list the following in the list file
@@ -29,8 +29,7 @@
 .ORG		0x0000					; next instruction written to address 0x0000
 									; first instruction of an executable always located at address 0x0000
 	rjmp Reset						; the reset vector
-;	rjmp ADC_INT						; ADC Conversion Complete Handler
-	
+	rjmp TIM0_int					; TIMER0_COMPA interrupt
 
 /******************** Reset vector */
 
@@ -55,18 +54,18 @@ Reset:
 
 	ProgramLoop:
 		
-		ldi r16, 0
+		ldi r16, 0						; read channel 0
 		call readADC
 		lds r16, ADCH
-		
-		clr r18
-		shiftLoop:
-			lsr r16							; equivalent to left shifting r27:r26 by 7
-			inc r18
-			cpi r18, 5
-		brlt shiftLoop
-
-		rcall LightDisplay					; r16 now holds the 3-bit reading
+		com r16
+		out OCR0A, r16					; set the timer to the 1's complement of the 8-bit ADC reading
+/*		
+		ser r16
+		clr r15
+		out PORTB, r16
+		rcall startTimer
+		out PORTB, r15
+		rcall startTimer	*/
 
 	jmp ProgramLoop
 
@@ -74,6 +73,31 @@ Reset:
 
 
 /********************* Subroutines */
+
+/************* Init Timer/Counter0 */
+
+initTimer :
+
+	ldi r16, (1 << CS01)
+	ldi r17, (1 << CS00)
+	or r17, r16
+	in r16, TCCR0B
+	or r17, r16
+	out TCCR0B, r17						; TCCR0B |= (1 << CS01) | (1 << CS00) for /64 prescalar 
+										; OR TRY: TCCR0B |= (1 << CS01) for /8 prescalar 
+	ldi r16, (1 << OCIE0A)
+	lds r17, TIMSK0
+	or r17, r16
+	sts TIMSK0, r17						; TIMSK0 |= (1 << OCIE0A) TIM0 compare interrupts
+
+	ldi r16, (1 << TOIE0)
+	lds r17, TIMSK0
+	or r17, r16
+	sts TIMSK0, r17						; TIMSK0 |= (1 << TOIE0); overflow interrupt enable			
+	
+	SEI									; Enable global interrrupts
+									
+ret
 
 
 /********************** GPIO Inits */
@@ -124,7 +148,7 @@ testGPIO :
 	PUSH r16
 	
 	; Flash PortB a few times
-	ldi r16, 4
+	ldi r16, 3
 	BlinkLoop:
 		ser r17
 		out PORTB, r17
@@ -230,178 +254,27 @@ ret
 
 
 
-/******************** Dispaly Reading */
-
-; Pre:		The 3-bit value is in r16
-LightDisplay :
-
-	PUSH r15
-
-	clr r15
-	out PORTB, r15
-
-	cpi r16, 1
-	brge pin1
-	rjmp next1
-	pin1:
-		sbi PORTB, 0
-	
-	next1:
-	cpi r16, 2
-	brge pin2
-	rjmp next2
-	pin2:
-		sbi PORTB, 1
-
-	next2:
-	cpi r16, 3
-	brge pin3
-	rjmp next3
-	pin3:
-		sbi PORTB, 2
-
-	next3:
-	cpi r16, 4
-	brge pin4
-	rjmp next4
-	pin4:
-		sbi PORTB, 3
-
-	next4:
-	cpi r16, 5
-	brge pin5
-	rjmp next5
-	pin5:
-		sbi PORTB, 4
-
-	next5:
-	cpi r16, 6
-	brge pin6
-	rjmp next6
-	pin6:
-		sbi PORTB, 5
-
-	next6:
-	cpi r16, 7
-	brge pin7
-	rjmp next7
-	pin7:
-		sbi PORTB, 6
-
-	next7:
-	cpi r16, 8
-	brge pin8
-	rjmp next8
-	pin8:
-		sbi PORTB, 7
-
-	next8:
-
-	POP r15
-
-ret
-
-
-
-
-/*
-
-	Command : Cycle : Description
-
-	ldi		: 1 : Load Immediate Into; Loads an 8-bit constant directly to regs.16 to 31.
-
-	cbi		: 1 : Clear Bit In I/O Register — Clears a specified bit in an I/O register.
-
-	sbi		: 1 : Set Bit in I/O Register — Sets a specified bit in an I/O Register.
-
-	out		: 1 : Store Register to I/O Location — Stores data from register Rr in the 
-					Register File to I/O Space(Ports, Timers, Configuration Registers, etc.).
-
-	dec		: 1 : Decrement — Subtracts one from the contents of register Rd and 
-					places the result in the destination register Rd.
-
-	adiw	: 2 : Add Immediate to Word — Adds an immediate value (0–63) 
-					to a register pair and places the result in the register pair.
-
-	brne	: 2 : Branch if Not Equal — Conditional relative branch. 
-					Tests the Zero Flag (Z) and branches relatively to PC if Z is cleared.
-
-	rcall	: 1 : Relative Call to Subroutine — Relative call to an address within PC
-
-	ret		: 1 : Return from Subroutine — Returns from the subroutine.
-
-	rjmp	: 1 : Relative Jump — Relative jump to an address.
-
-	sts		: 2 : Stores one byte from a Register to the data space.  For parts with SRAM, 
-					the data space consists of the Register File, I/O memory, 
-					and internal SRAM (and external SRAM if applicable).
-
-*/
-
-
-
-
-
-
-
-
 
 
 /****** Interrupt service routines */
-/*
-ADC_INT:
-	
+
+TIM0_int: 
+
 	; Prologue
 	PUSH r17							; save register on stack
 	PUSH r18
-	PUSH r26
-	PUSH r27
-	IN r17,SREG							; save flags
+	PUSH r19
+	IN r17,SREG	
 
-	lds r27, ADCH
-	lds r26, ADCL
-	
-	; 2^10		= 1024
-	; 1024/128	= 8
-	; 2^7		= 128
-	
-	clr r18								; iterator
-	shiftLoop:
-		lsr r27							; shift r27:r26 left by 7
-		inc r18
-		cpi r18, 7
-		brlt shiftLoop
-
-	clr r18
-	out PORTB, r18								
-	lightLoop:
-		sbi PORTB, r26					; operand 2 of sbi must be constant
-		cpi r26, 0
-		brge lightLoop
-	
-	mov r16, r26
-	call LightDisplay
-	
+	in r18, PORTB
+	ser r19
+	eor r18, r19
+	out PORTB, r18						; toogle all output on PORTB
 
 	; Epilogue
 	OUT SREG,r17						; restore flags
-	POP r27
-	POP r26
-	POP r18
+	pop r19
+	pop r18
 	POP r17
 
-RETI								; end of service routine
-
-/***************** Interrupt Inits /
-
-initInterrupts:
-
-	ldi r16, (1 << ADIE)
-	sts ADCSRA, r16						; Activate ADC Conversion Complete Interrupt
-	
-	SEI									; SREG = SREG | (1 << I)
-
-ret
-
-*/
-
+reti
