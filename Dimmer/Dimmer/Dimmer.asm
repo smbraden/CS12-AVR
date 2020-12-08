252c78be4d5f8eedd89b980991e338eafbbc77c6
smbraden <smbraden@ucsc.edu>
2020-12-08 15:05:25 -0800

    edits to ISR dimmer.
    It's close, but timing is off between ADC reading and duty cycle reset. Perhaps an issue with the clock prescalars

--- Dimmer/Dimmer/Dimmer.asm
+++ Dimmer/Dimmer/Dimmer.asm
@@ -19,8 +19,9 @@
 .EQU		FALSE = 0x00
 .EQU		DEFAULT_DELAY = 3
 
-.DEF		ADC_SAMPLE = r21		; don't use r21 anywhere else
-.DEF		ADC_FLAG = r22			; don't use r21 anywhere else
+.DEF		ADC_SAMPLE	= r19
+.DEF		F_MASK		= r20
+.DEF		NULL_MASK	= r21
 
 /******************** SRAM defines */
 
@@ -34,31 +35,9 @@
 									; first instruction of an executable always located at address 0x0000
 	
 	jmp Reset				; RESET
-	reti					; INT0
-	reti					; INT1
-	reti					; PCINT0
-	reti					; PCINT1
-	reti					; PCINT2
-	reti					; WDT
-	reti					; TIMER2_COMPA
-	reti					; TIMER2_COMPB
-	reti					; TIMER2_OVF
-	reti					; TIMER1_CAPT
-	reti					; TIMER1_COMPA
-	reti					; TIMER1_COMPB
-	reti					; TIMER1_OVF
 	jmp TIMER0_COMPA		; TIMER0_COMPA
-	reti					; TIMER0_COMPB
 	jmp TIMER0_OVF			; TIMER0_OVF
-	reti					; SPI_STC
-	reti					; USART_RX
-	reti					; USART_UDRE
-	reti					; USART_TX
-	jmp ADC_CONV			; ADC
-	reti					; EE_READY
-	reti					; ANALOG_COMP
-	reti					; TWI
-	reti					; SPM_Ready
+	
 
 /******************** Reset Routine */
 
@@ -71,37 +50,31 @@ Reset:
 	ldi r16, LOW(RAMEND)		
 	out SPL, r16						; SPL = "Stack Pointer Low"
 
-	/**************** Initialize other */
+	/************ Initialize Named reg */
+	
+	ser F_MASK							; 0xFF
+	clr NULL_MASK						; 0x00
 
+	/************* Initialize hardware */
+
+;	ldi r16, 0							; read channel 0
+;	rcall initContiuousADC				; prepare channel to sample continuously
+	rcall initSingleSampleADC
 	rcall initGPIO
-	ldi r16, 0							; read channel 0
-	rcall initSingleSampleADC		; prepare channel to sample continuously
 	rcall testGPIO
 	rcall initTimer
 
-	ldi ADC_FLAG, FALSE
-	ldi r16, DEFAULT_DELAY
 	sei									; Enable global interrrupts
 
 	/************** Begin Program Loop */
 	
 	ProgramLoop:
-		
-		call Delay_ms_byte					; delay for DEFAULT_DELAY milliseconds
+	
 	jmp ProgramLoop
 
 
 
 /****** Interrupt service routines */
-
-ADC_CONV:
-	
-	ldi ADC_FLAG,  TRUE
-	lds ADC_SAMPLE, ADCH
-
-reti
-
-
 
 
 /***************** PWM Timer ISR's */
@@ -112,15 +85,12 @@ TIMER0_COMPA:
 
 	; Prologue
 	push r17							; save register on stack
-	push r18
 	in r17,SREG	
-		
-	clr r18
-	out PORTB, r18						; PORTB all off 
+	
+	out PORTB, NULL_MASK				; PORTB all off 
 
 	; Epilogue
 	out SREG,r17						; restore flags
-	pop r18
 	pop r17
 
 reti
@@ -132,20 +102,28 @@ reti
 TIMER0_OVF :
 
 	; Prologue
-	push r18							; save register on stack
 	push r17
 	in r17,SREG	
-
-	ser r18
-	out PORTB, r18						; PORTB all on 
-
+	
+	;******* Single conversion mode
+	
+	ldi r16, 0	
+	call readADC
+	lds ADC_SAMPLE, ADCH
 	out OCR0A, ADC_SAMPLE				; set the new duty cycle
+	out PORTB, F_MASK					; PORTB all on 
+	
+	;******* Continuous mode
+	/*
+	out PORTB, F_MASK					; PORTB all on
+	lds ADC_SAMPLE, ADCH				; set the new duty cycle
+	out OCR0A, ADC_SAMPLE				; set the new duty cycle
+	*/
 
 	; Epilogue
 	out SREG,r17						; restore flags
 	pop r17
-	pop r18
-	
+		
 reti
 
 
@@ -158,17 +136,13 @@ reti
 
 initGPIO:
 
-	push r16
 										; Output 
-	ser r16								; set all bits in register
-	out DDRB, r16						; entire PORTB set to output
+	out DDRB, F_MASK					; entire PORTB set to output
 	
 										; Voltage sensor
 	cbi DDRC, PC0						; PC0 set to input
 	sbi PORTC, PC0						; activaate pull-up resistors
 
-	pop r16
-	
 ret
 
 
@@ -176,36 +150,46 @@ ret
 
 /********************** ADC Inits */
 
-initSingleSampleADC:
 
-	push r15
-	push r16
+; Pre:		The channel to be read is in r16 (a 3-bit value, 0-8)
+initContiuousADC:
 
-	ldi r16, (1 << REFS0)			; set REFS0 bit without disturbing other bits
-	lds r15, ADMUX
-	or r15, r16
-	ldi r16, (1 << REFS1)			; clear REFS1 bit without disturbing other bits
-	com r16
-	and r15, r16
-	sts ADMUX, r15					; Voltage Ref set to AVCC w/ external cap at AREF pin
-
-
-	ldi r16, (1 << ADLAR)			
-	lds r15, ADMUX
-	or r15, r16
-	sts ADMUX, r15					; left-adjusted conversion in the ADCH:ADCL
+	push r17
 	
+	lds r17, ADMUX
+	and r17, r16					; read the argument channel
+	sbr r17, (1 << REFS1)			
+	cbr r17, (1 << REFS0)			; Voltage Ref set to AVCC w/ external cap at AREF pin
+	sbr r17, (1 << ADLAR)			; left-adjusted conversion in the ADCH:ADCL
+	sts ADMUX, r17					
 
-	ldi r16, (1 << ADPS2) | (1 << ADPS1)
-	lds r15, ADCSRA
-	or r15, r16
-	sts ADCSRA, r15					; ADC prescalar division factor set to 64
+	lds r17, ADCSRB
+	cbr r17, (1 << ADTS2) | (1 << ADTS1) | (1 << ADTS0) 
+	sts ADCSRB, r17					; Auto-trigger source: free-running mode
 
-	ldi r16, (1 << ADEN)
-	sts ADCSRA, r16					; Set ADC enable bit
+									; ADCSRA |= (1 << ADEN) ... ADC enable bit
+									; ADCSRA |= (1 << ADATE) ... ADC Auto Trigger Enable
+									; ADCSRA |= (1 << ADIE) ...   ADC Interrupt Enable
+	lds r17, ADCSRA
+	sbr r17, (1 << ADEN) | (1 << ADATE) | ( 1 << ADIE)
+	
+	sbr r17, (1 << ADPS2) | (1 << ADPS1)	; prescalar division factor 64
+	cbr r17, (1 << ADPS0)
+	
+	sts ADCSRA, r17							
+	/*
+	ADPS2	ADPS1	ADPS0	Division Factor
+	0		0		0		2
+	0		0		1		2
+	0		1		0		4
+	0		1		1		8
+	1		0		0		16
+	1		0		1		32
+	1		1		0		64
+	1		1		1		128
+	*/
 
-	pop r16
-	pop r15
+	pop r17
 
 ret
 
@@ -213,48 +197,35 @@ ret
 
 
 
-; Pre:		The channel to be read is in r16 (a 3-bit value, 0-8)
-initContiuousADC:
+initSingleSampleADC:
 
 	push r17
-	push r16
-	
-	lds r17, ADMUX
-	and r17, r16					; all upper bits in ADMUX are set below
-	sts ADMUX, r17					; read the argument channel
 
-	ser r17
-	cbr r17, (1 << ADTS2) | (1 << ADTS1) | (1 << ADTS0) 
-	lds r16, ADCSRB
-	and r17, r16
-	sts ADCSRB, r17					; Auto-trigger source: free-running mode
-	
-	ldi r16, (1 << REFS1)
 	lds r17, ADMUX
-	or r17, r16
-	cbr r17, REFS0
-	sts ADMUX, r17					; Voltage Ref set to AVCC w/ external cap at AREF pin
+	sbr r17, (1 << REFS0)
+	cbr r17, (1 << REFS1)			; Voltage Ref set to AVCC w/ external cap at AREF pin
+	sbr r17, (1 << ADLAR)			; left-adjusted conversion in the ADCH:ADCL
+	sts ADMUX, r17
+		
 	
-	ldi r16, (1 << ADLAR)			
-	lds r17, ADMUX
-	or r17, r16
-	sts ADMUX, r17					; left-adjusted conversion in the ADCH:ADCL
-
-									; ADCSRA |= (1 << ADEN) ... ADC enable bit
-									; ADCSRA |= (1 << ADATE) ... ADC Auto Trigger Enable
-									; ADCSRA |= (1 << ADIE) ...   ADC Interrupt Enable
-
-	ldi r16, (1 << ADEN) | (1 << ADATE) | ( 1 << ADIE)
 	lds r17, ADCSRA
-	or r17, r16						
+
+	sbr r17, (1 << ADPS1)				; ADC prescalar division factor set to 4
+	cbr r17, (1 << ADPS2) | (1 << ADPS0)
+	sbr r17, (1 << ADEN)				; Set ADC enable bit
 	sts ADCSRA, r17					
+	/*
+	ADPS2	ADPS1	ADPS0	Division Factor
+	0		0		0		2
+	0		0		1		2
+	0		1		0		4
+	0		1		1		8
+	1		0		0		16
+	1		0		1		32
+	1		1		0		64
+	1		1		1		128
+	*/
 
-	; ADCSRA |= (1 << ADPS2) ... prescalar division factor set to 128
-	ldi r16, (1 << ADPS2) | (1 << ADPS1)| (1 << ADPS0)
-	or r17, r16						
-	sts ADCSRA, r17								
-
-	pop r16
 	pop r17
 
 ret
@@ -269,29 +240,25 @@ ret
 ; Post:		The conversion awaits in ADCH and ACDL
 readADC:
 
-	PUSH r18
 	PUSH r17
-
+	PUSH r18
 
 	lds r17, ADMUX
 	or r17, r16
 	sts ADMUX, r17
 
-
-	ldi r17, (1 << ADSC)				
-	lds r18, ADCSRA
-	or r17, r18
+	lds r17, ADCSRA
+	sbr r17, (1 << ADSC)
 	sts ADCSRA, r17						; ADCSRA |= (1 << ADSC);
 
-	lds r17, ADCSRA
-	cbr r17, ADSC						; sample of ADSC bit cleared from ADCSRA
-
+	cbr r17, (1 << ADSC)				; sample of ADSC bit cleared from ADCSRA
+	
 	loopUntilClear:						; ADCSRA out of range for sbic command
 		lds r18, ADCSRA
 		cp r18, r17
 			breq exit
-		jmp loopUntilClear
-
+		jmp loopUntilClear				; (*) Refactoring note
+	
 	exit:
 
 	POP r18
@@ -299,6 +266,13 @@ readADC:
 
 ret
 
+/*
+	
+(*)	loopUntilClear:
+		cpi r17, ADCSRA
+			breq exit
+		jmp loopUntilClear
+*/
 
 
 
@@ -307,22 +281,43 @@ ret
 initTimer:
 
 	PUSH r17
-	PUSH r16
-
-	ldi r17, (1 << CS01) | (1 << CS00)
-	in r16, TCCR0B
-	or r17, r16
-	out TCCR0B, r17						; clock division by prescalar of 64
-										
-	ldi r16, (1 << OCIE0A) | (1 << TOIE0)
-	lds r17, TIMSK0						; TIMSK0 |= (1 << TOIE0) overflow interrupt enable
-	or r17, r16							; TIMSK0 |= (1 << OCIE0A) TIM0 output compare interrupts
+	
+	in r17, TCCR0B
+	sbr r17, (1 << CS01) | (1 << CS00)
+	cbr r17, (1 << CS02)
+	out TCCR0B, r17							; clock division prescalar set to 64
+							
+	/*
+	CS02	CS01	CS00	Description
+	0		0		0		No clock source (Timer/Counter stopped)
+	0		0		1		clkI/O/(No prescaling)
+	0		1		0		clkI/O/8 (From prescaler)
+	0		1		1		clkI/O/64 (From prescaler)
+	1		0		0		clkI/O/256 (From prescaler)
+	1		0		1		clkI/O/1024 (From prescaler)
+	1		1		0		External clock source on T0 pin. Clock on falling edge.
+	1		1		1		External clock source on T0 pin. Clock on rising edge
+	*/						
+						
+	lds r17, TIMSK0							; TIMSK0 |= (1 << TOIE0) overflow interrupt enable
+	sbr r17, (1 << OCIE0A) | (1 << TOIE0)	; interrupt for compare register A
+	cbr r17, (1 << OCIE0B)					; no interrupt for compare register B
 	sts TIMSK0, r17
 	
-	POP r16
 	POP r17					
 			
 ret
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -332,7 +327,6 @@ testGPIO :
 	
 	; PUSH r27
 	; PUSH r26
-	PUSH r19
 	PUSH r18
 	PUSH r17
 	PUSH r16
@@ -361,24 +355,21 @@ testGPIO :
 	ldi r18, 0
 	RepeatShifts:						; Traverse the port a few times
 
-	ldi r19, (1 << 0)
-	ldi r17, 0
+	ldi r17, (1 << 0)
 	
 	LeftShiftLoop:
-		out PORTB, r19
-		lsl r19
+		out PORTB, r17
 		rcall Delay_ms_byte
-		inc r17
-		cpi r17, 7
-	brlt LeftShiftLoop	
+		lsl r17
+		cpi r17, (1 << 7)
+	brne LeftShiftLoop	
 	
 	RightShiftLoop:
-		out PORTB, r19
-		lsr r19
+		out PORTB, r17
 		rcall Delay_ms_byte
-		inc r17
-		cpi r17, 14
-	brlt RightShiftLoop	
+		lsr r17
+		cpi r17, (1 << 0)
+	brne RightShiftLoop	
 
 	inc r18
 	cpi r18, 2
@@ -390,7 +381,6 @@ testGPIO :
 	POP r16
 	POP r17
 	POP r18
-	POP r19
 	; POP r26
 	; POP r27
 
@@ -472,7 +462,6 @@ Delay_us_word:
 	push r26
 	push r27
 	
-
 	microLoop:
 	sbiw r26, 1
 	brne microLoop
